6e3b96ac397bb7b4ea7d76e361b3b38229c785c1
Yuri-m-b <yuri925@hotmail.com>
2026-07-01 11:08:34 -0300

    [Upgrade] First window

--- interface/interface.py
+++ interface/interface.py
@@ -1,26 +1,65 @@
 """Interface generator for the interface"""
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QPushButton,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
+from PySide6.QtCore import Slot, Qt
+from PySide6.QtGui import QPixmap
+
+FIGURE_MAPPING = {
+    "figure1": "images/cats_1.png",
+}
 
 
 class MainWindow(QMainWindow):
-    """Class representing the main window of the application."""
+    """Main window class for the application."""
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Hello, World!")
 
-        button = QPushButton("Exit")
+        self.unsetCursor()
+
+        button = QPushButton("Te amo!")
+        button.setFixedSize(200, 150)
         button.pressed.connect(self.close)
 
-        self.setCentralWidget(button)
+        figure_label = QLabel(self)
+        pixmap = QPixmap(FIGURE_MAPPING["figure1"])
+        figure_label.setPixmap(pixmap)
+        figure_label.setAlignment(Qt.AlignmentFlag.AlignBottom)
+        figure_label.size().setHeight(400)
+
+        content_layout = QVBoxLayout()
+        content_layout.addWidget(button, alignment=Qt.AlignmentFlag.AlignHCenter)
+        content_layout.addWidget(figure_label, alignment=Qt.AlignmentFlag.AlignBottom)
+        container = QWidget()
+        container.setLayout(content_layout)
+        self.setCentralWidget(container)
         self.show()
+
+
+@Slot()
+def say_hello():
+    """Test function"""
+    print("Button clicked, Hello!")
 
 
 def main():
     """Main function to run the application."""
     app = QApplication([])
+
     window = MainWindow()
+    window.resize(1200, 800)
+    with open("interface/interface.css", "r", encoding="utf-8") as file:
+        _style = file.read()
+        app.setStyleSheet(_style)
+
     app.exec()
 
 
