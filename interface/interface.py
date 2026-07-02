81f790433b67c9eed2b69a5bd3345c96830ed356
Yuri-m-b <yuri925@hotmail.com>
2026-07-02 00:54:58 -0300

    [pages] Add page 3 and 4

--- interface/interface.py
+++ interface/interface.py
@@ -11,12 +11,14 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
-from PySide6.QtCore import Slot, Qt
-from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QSize, Slot, Qt
+from PySide6.QtGui import QIcon, QPixmap
 
 FIGURE_MAPPING = {
     "figure1": "images/cats_1.png",
     "figure2": "images/cats_2.png",
+    "figure3": "images/i_love_you.png",
+    "figure4": "images/cats_3.png",
 }
 
 
@@ -34,9 +36,13 @@ class FirstPage(QWidget):
 
         layout = QVBoxLayout(self)
         pixmap = QPixmap(FIGURE_MAPPING["figure1"])
+        pixmap2 = QPixmap(FIGURE_MAPPING["figure3"])
         figure_label = QLabel(self)
         figure_label.setPixmap(pixmap)
+        figure_label2 = QLabel(self)
+        figure_label2.setPixmap(pixmap2)
         layout.addWidget(figure_label, alignment=Qt.AlignmentFlag.AlignLeft)
+        layout.addWidget(figure_label2, alignment=Qt.AlignmentFlag.AlignRight)
 
         text_label = QLabel("Para a mulher da minha vida!!", self)
         text_label.setObjectName("title")
@@ -73,7 +79,7 @@ class SecondPage(QWidget):
         sim_btn = QPushButton("Sim", self)
         sim_btn.resize(250, 50)
         sim_btn.move(500, 400)
-        sim_btn.clicked.connect(QApplication.instance().quit)
+        sim_btn.clicked.connect(lambda: self.stack.setCurrentIndex(2))
 
         nao_btn = QPushButton("Não", self)
         nao_btn.resize(250, 50)
@@ -101,7 +107,73 @@ class ThirdPage(QWidget):
     def init_ui(self):
         """Set up the UI for the third page."""
         layout = QVBoxLayout(self)
-        pixmap = QPixmap(FIGURE_MAPPING["figure2"])
+        pixmap = QPixmap(FIGURE_MAPPING["figure4"])
+        figure_label = QLabel(self)
+        figure_label.setPixmap(pixmap)
+        layout.addWidget(figure_label, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        text_label = QLabel("Qual imagem representa mais a gente?", self)
+        text_label.resize(500, 100)
+        text_label.move(600, 200)
+
+        button_1 = QPushButton(self)
+        button_1.resize(300, 300)
+        button_1.setStyleSheet("""
+            QPushButton {
+                border-image: url("images/us_1.png") 0 0 0 0 stretch stretch;
+                border: none;
+            }
+        """)
+        button_1.move(400, 300)
+        button_1.clicked.connect(lambda: self.stack.setCurrentIndex(3))
+
+        button_2 = QPushButton(self)
+        button_2.resize(300, 300)
+        button_2.setStyleSheet("""
+            QPushButton {
+                border-image: url("images/us_2.png") 0 0 0 0 stretch stretch;
+                border: none;
+            }
+        """)
+        button_2.move(800, 300)
+        button_2.clicked.connect(lambda: self.stack.setCurrentIndex(3))
+
+        button_3 = QPushButton(self)
+        button_3.resize(300, 300)
+        button_3.setStyleSheet("""
+            QPushButton {
+                border-image: url("images/us_3.png") 0 0 0 0 stretch stretch;
+                border: none;
+            }
+        """)
+        button_3.move(400, 600)
+        button_3.clicked.connect(lambda: self.stack.setCurrentIndex(3))
+
+        button_4 = QPushButton(self)
+        button_4.resize(300, 300)
+        button_4.setStyleSheet("""
+            QPushButton {
+                border-image: url("images/us_4.png") 0 0 0 0 stretch stretch;
+                border: none;
+            }
+        """)
+        button_4.move(800, 600)
+        button_4.clicked.connect(lambda: self.stack.setCurrentIndex(3))
+
+
+class FourthPage(QWidget):
+    """The fourth screen that loads next."""
+
+    def __init__(self, stack_widget):
+        """Initialize the fourth page and set up its UI."""
+        super().__init__()
+        self.stack = stack_widget
+        self.init_ui()
+
+    def init_ui(self):
+        """Set up the UI for the fourth page."""
+        layout = QVBoxLayout(self)
+        pixmap = QPixmap(FIGURE_MAPPING["figure4"])
         figure_label = QLabel(self)
         figure_label.setPixmap(pixmap)
         layout.addWidget(figure_label, alignment=Qt.AlignmentFlag.AlignLeft)
@@ -124,13 +196,13 @@ class MainWindow(QMainWindow):  # pylint: disable=too-few-public-methods
         self.page2 = SecondPage(self.stacked_widget)
         self.page3 = ThirdPage(self.stacked_widget)
         self.page4 = FourthPage(self.stacked_widget)
-        self.page5 = FifthPage(self.stacked_widget)
+        # self.page5 = FifthPage(self.stacked_widget)
 
         self.stacked_widget.addWidget(self.page1)
         self.stacked_widget.addWidget(self.page2)
         self.stacked_widget.addWidget(self.page3)
         self.stacked_widget.addWidget(self.page4)
-        self.stacked_widget.addWidget(self.page5)
+        # self.stacked_widget.addWidget(self.page5)
 
         self.stacked_widget.setCurrentIndex(0)
 
